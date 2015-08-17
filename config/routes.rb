a3007ca0b27bb271f718486ce82c906fe7a6b3a4
HectorMg <hectormgwake@gmail.com>
2015-08-17 18:56:50 -0500

    Add Static Pages controller and update Gemfile

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+
+  root 'static_pages#home'
+
+  get 'static_pages#home'
+
+  get 'static_pages#curriculum'
+
+  get 'static_pages#contact'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
