6325a357bac20310dd2b1f8a02d64eccb583f3e8
HectorMg <hectormgwake@gmail.com>
2015-08-20 17:59:58 -0500

    Add Static Pages tests

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,6 @@ Rails.application.routes.draw do
 
   get 'static_pages/contact'
 
+  get 'static_pages/facilities'
+
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,16 +4,25 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | CER Cirugía Plástica"
   end
 
   test "should get curriculum" do
     get :curriculum
     assert_response :success
+    assert_select "title", "Curriculum | CER Cirugía Plástica"
   end
 
   test "should get contact" do
     get :contact
     assert_response :success
+    assert_select "title", "Contact | CER Cirugía Plástica"
+  end
+
+  test "should get facilities" do
+    get :facilities
+    assert_response :success
+    assert_select "title", "Facilities | CER Cirugía Plástica"
   end
 
 end
