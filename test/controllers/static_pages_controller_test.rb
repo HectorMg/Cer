60a992ca984c401c1a0db4a74f9da6ce211ff5e3
HectorMg <hectormgwake@gmail.com>
2015-08-28 20:44:24 -0500

    Add setup for base title to eliminate repetition in the static pages controller test helper

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,28 +1,32 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+  def setup
+    @base_title = "CER Cirugía Plástica"
+  end
+
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | CER Cirugía Plástica"
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get curriculum" do
     get :curriculum
     assert_response :success
-    assert_select "title", "Curriculum | CER Cirugía Plástica"
+    assert_select "title", "Curriculum | #{@base_title}"
   end
 
   test "should get contact" do
     get :contact
     assert_response :success
-    assert_select "title", "Contact | CER Cirugía Plástica"
+    assert_select "title", "Contact | #{@base_title}"
   end
 
   test "should get facilities" do
     get :facilities
     assert_response :success
-    assert_select "title", "Facilities | CER Cirugía Plástica"
+    assert_select "title", "Facilities | #{@base_title}"
   end
 
 end
