8bf0334cd55e9fdcf15ee1ec6f10afc1e89ccd5d
HectorMg <hectormgwake@gmail.com>
2015-08-28 20:40:45 -0500

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,8 @@
 Rails.application.routes.draw do
 
-
-  get 'static_pages/home'
-
-  get 'static_pages/curriculum'
-
-  get 'static_pages/contact'
-
-  get 'static_pages/facilities'
+  root 'static_pages#home'
+  get  'static_pages/curriculum'
+  get  'static_pages/contact'
+  get  'static_pages/facilities'
 
 end
